ec1f19a39f35b03600da11dd74ad150b525323c3
proyecto <nataliepenalva123@gmail.com>
2023-02-19 22:52:36 -0300

    agregado de main js para la primera entrega

---
+++ main.js
@@ -0,0 +1,81 @@
+// Formulario de contacto
+
+// function contacto (){
+//     let nombreContacto = prompt ("Ingrese su nombre");
+//     if(nombreContacto === ""){
+//         alert ("No ingreso el nombre");
+//     }else{
+//         alert(`Su nombre es ${nombreContacto}`);
+//     }
+//     let apellidoContacto = prompt("Ingrese su apellido");
+//     if(apellidoContacto === ""){
+//         alert ("No ingreso el apellido");
+//     }else{
+//         alert(`Su apellido es ${apellidoContacto}`);
+//     }
+//     let emailContacto = prompt ("Ingrese su email");
+//     if(emailContacto === ""){
+//         alert("No ingreso su email");
+//     }else{
+//         alert(`Su email es ${emailContacto}`);
+//     }
+//     let telefonoContacto = parseInt(prompt("Ingrese su telefono"));
+//     if(telefonoContacto ===  NaN){
+//         alert ("No ingreso su telefono");
+//     }else{
+//         alert (`Su telefono es ${telefonoContacto}`)
+//     }
+//     let consulta = prompt ("Ingrese su consulta");
+//     if(consulta === ""){
+//         alert("No ingreso su consulta");
+//     }
+//     else{
+//         alert(`Su consulta es ${consulta}`)
+//     }
+// }
+// contacto()
+
+
+
+
+// carrito de compras
+// let productos = prompt("Agregue sus productos al carrito");
+// let velador ="velador";
+// let juegoDeSillas= "juego de sillas";
+// let cuadro = "cuadro";
+// let biblioteca= "biblioteca";
+// let precioVelador = 5600;
+// let precioJuegoDeSillas = 27880;
+// let precioCuadro = 3000;
+// let precioBiblioteca = 45900;
+
+//  const suma = (a,b) => a + b
+
+// while(productos != ""){
+//     switch(productos){
+//         case "velador":
+//             alert(`Agrego ${velador} al carrito`);
+//             alert (precioVelador);
+//           break;
+//         case "juego de sillas":
+//             alert(`Agrego ${juegoDeSillas} al carrito`);
+//              alert(precioJuegoDeSillas);
+//             break;
+//         case "cuadro":
+//             alert(`Agrego ${cuadro} al carrito`);
+//             alert(precioCuadro);
+//             break;
+//         case "biblioteca":
+//             alert(`Agrego ${biblioteca} al carrito`);
+//             alert(precioBiblioteca);
+//             break;
+//         default:
+//             alert("Ese producto no se encuentra disponible");
+//             break;
+//     }
+//     productos = prompt("Agregue sus productos al carrito");
+
+//     let nuevoPrecio = suma(precioBiblioteca, precioCuadro);
+//     alert (nuevoPrecio);
+// }
+
