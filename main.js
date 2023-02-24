86a79734fbb68b2f1f0468fabede6aef3c052e66
proyecto <nataliepenalva123@gmail.com>
2023-02-24 11:11:31 -0300

    modificación en la suma de productos

--- main.js
+++ main.js
@@ -39,43 +39,37 @@
 
 
 // carrito de compras
-// let productos = prompt("Agregue sus productos al carrito");
-// let velador ="velador";
-// let juegoDeSillas= "juego de sillas";
-// let cuadro = "cuadro";
-// let biblioteca= "biblioteca";
-// let precioVelador = 5600;
-// let precioJuegoDeSillas = 27880;
-// let precioCuadro = 3000;
-// let precioBiblioteca = 45900;
+let productos = prompt("Agregue sus productos al carrito");
+let precioVelador = 5600;
+let precioJuegoDeSillas = 27880;
+let precioCuadro = 3000;
+let precioBiblioteca = 45900;
 
-//  const suma = (a,b) => a + b
+ const suma = (a,b) => a + b;
+ let total = 0;
 
-// while(productos != ""){
-//     switch(productos){
-//         case "velador":
-//             alert(`Agrego ${velador} al carrito`);
-//             alert (precioVelador);
-//           break;
-//         case "juego de sillas":
-//             alert(`Agrego ${juegoDeSillas} al carrito`);
-//              alert(precioJuegoDeSillas);
-//             break;
-//         case "cuadro":
-//             alert(`Agrego ${cuadro} al carrito`);
-//             alert(precioCuadro);
-//             break;
-//         case "biblioteca":
-//             alert(`Agrego ${biblioteca} al carrito`);
-//             alert(precioBiblioteca);
-//             break;
-//         default:
-//             alert("Ese producto no se encuentra disponible");
-//             break;
-//     }
-//     productos = prompt("Agregue sus productos al carrito");
-
-//     let nuevoPrecio = suma(precioBiblioteca, precioCuadro);
-//     alert (nuevoPrecio);
-// }
-
+while(productos != ""){
+    switch(productos){
+        case "velador":
+            alert(`Agrego velador al carrito`);
+           total = suma(total, precioVelador);
+          break;
+        case "juego de sillas":
+            alert(`Agrego juego de sillas al carrito`);
+             total = suma(total, precioJuegoDeSillas);
+            break;
+        case "cuadro":
+            alert(`Agrego cuadro al carrito`);
+            total = suma(total, precioCuadro);
+            break;
+        case "biblioteca":
+            alert(`Agrego biblioteca al carrito`);
+            total = suma(total, precioBiblioteca);
+            break;
+        default:
+            alert("Ese producto no se encuentra disponible");
+            break;
+    }
+    productos = prompt("Agregue sus productos al carrito");
+}
+alert(`El total de su compra es ${total}`)
